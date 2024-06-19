d96b34d46d7bdedef6effffaf2b7352e42eb43a8
rshelar21 <rshelar796@gmail.com>
2024-06-20 00:41:21 +0530

    removed samesite

--- controller/orderHistory.js
+++ controller/orderHistory.js
@@ -5,11 +5,11 @@ const orderData = async (req, res) => {
   try {
     const user = req.user;
     console.log(user);
-    const product = await orderModel.find({userId : user?.id});
+    const product = await orderModel.find({ userId: user?.id });
     if (!product.length)
       return res
         .status(200)
-        .json({ message: "No product found", result: false, product : [] });
+        .json({ message: "No product found", result: false, product: [] });
     res.status(200).json({ product, result: true, message: "Product found" });
   } catch (error) {
     res.status(500).json({ message: "Something went wrong", result: false });
--- controller/user.js
+++ controller/user.js
@@ -67,7 +67,7 @@ const login = async (req, res) => {
     res.cookie("authcookie", jwtToken, {
       maxAge: new Date(Date.now() + 2589200000),
       httpOnly: true,
-      sameSite: "None",
+      // sameSite: "none",
     });
 
     res.status(200).json({
@@ -118,7 +118,7 @@ const register = async (req, res) => {
     res.cookie("authcookie", jwtToken, {
       maxAge: new Date(Date.now() + 2589200000),
       httpOnly: true,
-      sameSite: "None",
+      // sameSite: "none",
     });
 
     res.status(200).json({
